a12772189804cc8c4cb182b5ba243159e5d37940
cybersunt <juliam2007@mail.ru>
2022-03-08 14:33:02 +0300

    feat: добавлены функции для генерации случайных чисел

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const getRandomInt = (min, max, quantity) => (Math.random() * (max - min) + min).toFixed(quantity);
+
+//функция валидации
+const getRandomValidate = (min, max, quantity) => {
+  if(min>max){
+    const change=min;
+    min=max;
+    max=change;
+  }
+  if(min===max){
+    // eslint-disable-next-line no-console
+    console.log('Числа от и до не должны быть равными!');
+  }
+  if(min<0 || max<0){
+    // eslint-disable-next-line no-console
+    console.log('Числа должны быть положительными!');
+  }
+  return getRandomInt(min, max, quantity);
+};
+
+getRandomValidate(0, 20, 1);
+console.log(getRandomValidate(1, 10, 3));
