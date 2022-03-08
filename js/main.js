bef448599669f7fa6e6031d6bd1b8a7a60e78ac9
cybersunt <juliam2007@mail.ru>
2022-03-08 14:34:47 +0300

    fix: eslint

--- js/main.js
+++ js/main.js
@@ -2,16 +2,16 @@ const getRandomInt = (min, max, quantity) => (Math.random() * (max - min) + min)
 
 //функция валидации
 const getRandomValidate = (min, max, quantity) => {
-  if(min>max){
-    const change=min;
+  if ( min > max ){
+    const change = min;
     min=max;
     max=change;
   }
-  if(min===max){
+  if (min === max ){
     // eslint-disable-next-line no-console
     console.log('Числа от и до не должны быть равными!');
   }
-  if(min<0 || max<0){
+  if (min < 0 || max < 0 ){
     // eslint-disable-next-line no-console
     console.log('Числа должны быть положительными!');
   }
@@ -19,4 +19,5 @@ const getRandomValidate = (min, max, quantity) => {
 };
 
 getRandomValidate(0, 20, 1);
+// eslint-disable-next-line no-console
 console.log(getRandomValidate(1, 10, 3));
