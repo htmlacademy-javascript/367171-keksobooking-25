3b77e3bb174e84940616d6b88070dcb2456275ab
cybersunt <juliam2007@mail.ru>
2022-03-09 11:02:00 +0300

    feat: добавлены моковые данные

--- js/main.js
+++ js/main.js
@@ -1,23 +1,6 @@
-const getRandomInt = (min, max, quantity) => (Math.random() * (max - min) + min).toFixed(quantity);
+import { data } from './data.js';
 
-//функция валидации
-const getRandomValidate = (min, max, quantity) => {
-  if ( min > max ){
-    const change = min;
-    min=max;
-    max=change;
-  }
-  if (min === max ){
-    // eslint-disable-next-line no-console
-    console.log('Числа от и до не должны быть равными!');
-  }
-  if (min < 0 || max < 0 ){
-    // eslint-disable-next-line no-console
-    console.log('Числа должны быть положительными!');
-  }
-  return getRandomInt(min, max, quantity);
-};
+const currentData = data.slice(0, 10);
 
-getRandomValidate(0, 20, 1);
 // eslint-disable-next-line no-console
-console.log(getRandomValidate(1, 10, 3));
+console.log(currentData);
